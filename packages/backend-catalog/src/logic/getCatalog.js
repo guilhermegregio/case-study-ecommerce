12d60b70c28eed07eb321cd604ce1b45b7f89fb0
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2018-09-10 23:10:53 -0300

    feat(backend-catalog) Merge games of all stores

--- packages/backend-catalog/src/logic/getCatalog.js
+++ packages/backend-catalog/src/logic/getCatalog.js
@@ -1,4 +1,8 @@
 const Promise = require('bluebird')
+const unionBy = require('lodash.unionby')
+const omit = require('lodash.omit')
+const pick = require('lodash.pick')
+const find = require('lodash.find')
 
 const getCatalog = ({act}) =>
   function(msg, done) {
@@ -21,7 +25,22 @@ const getCatalog = ({act}) =>
 
     Promise.all(commands)
       .then(([steam, xbox, psn]) => {
-        done(null, [].concat(steam, xbox, psn))
+        const games = [...steam, ...xbox, ...psn]
+
+        const uniqGames = unionBy(games, 'title').map(game => {
+          const brands = {}
+          const findInSteam = find(steam, ['title', game.title])
+          const findInXbox = find(xbox, ['title', game.title])
+          const findInPsn = find(psn, ['title', game.title])
+
+          brands['steam'] = findInSteam && pick(findInSteam, ['sku', 'price'])
+          brands['xbox'] = findInXbox && pick(findInXbox, ['sku', 'price'])
+          brands['psn'] = findInPsn && pick(findInPsn, ['sku', 'price'])
+
+          return Object.assign({}, omit(game, ['sku', 'price']), {brands})
+        })
+
+        done(null, Object.values(uniqGames))
       })
       .catch(err => console.log(err))
   }
